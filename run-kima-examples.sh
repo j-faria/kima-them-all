90f9d8a217a8e857e3e8229401fc48acc479969a
João Faria <joao.faria@astro.up.pt>
2019-04-10 23:20:32 +0100

    wrong paths

--- run-kima-examples.sh
+++ run-kima-examples.sh
@@ -2,18 +2,20 @@
 
 echo "running kima examples"
 
+cd $SHIPPABLE_BUILD_DIR/kima
 cd examples
 
 for e in 51Peg #BL2009 CoRoT7
 do
     cd $e
     pwd
-    
-    $HOME/run
+
+    $SHIPPABLE_BUILD_DIR/run
     
     # report
-    jupyter nbconvert --to notebook $HOME/templates/report.ipynb --output-dir .
+    jupyter nbconvert --to notebook $SHIPPABLE_BUILD_DIR/templates/report.ipynb --output-dir .
     jupyter nbconvert --to html --execute report.ipynb
 
     cd ..
+    
 done
